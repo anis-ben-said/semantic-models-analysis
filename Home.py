4cdbed57bb2558be5d98ca3dc6506f8c82828cac
anis <anis.ben-said@renault.com>
2024-08-07 22:05:34 +0200

    delete old imports from home

--- Home.py
+++ Home.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from modules.repo_utils import repo_exists, get_last_update_date, clone_or_update_repo,check_local_repo_encoding
 
 def main():
     st.title("Semantic Aspect Models analysis")
